dac61d9321a1afb2f55ae24eaa64c237c885306f
Menaid <menaidnocic@live.se>
2023-09-22 11:29:41 +0200

    First commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿int[] numbers = { 5, 7, 8, 6 };
+
+//foreach (int n in numbers)
+//{
+//    if (n == 5)
+//    {
+//        console.writeline("gött");
+//    }
+//    else if (n == 7 || n == 6)
+//    {
+//        console.writeline("inte gött");
+//    }
+//    else
+//    {
+//        console.writeline(n);
+//    }
+//}
+
+for (int i = 5; i > 0; i = 0)
+{
+    Console.WriteLine(i);
+}
+
+// ctrl + k + c = kommenterar allt markerat
+// ctrl + k + u = tar bort kommentarer på det som är markerat
